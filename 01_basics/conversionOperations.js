914b811ea66803d5b990af72d6abb627eb69e832
Abhishek Sunil Ghodke <abhighodke3@gmail.com>
2023-10-18 14:29:02 +0000

    Commit

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -1,9 +1,26 @@
-let score = "Abhishek";
+// let score = "Abhishek";
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-score = "33"
+// score = "33"
 
-console.log(Number(score))
-console.log(typeof(score))
+// console.log(Number(score))
+// console.log(typeof(score))
+
+let name = "Abhishek"
+let Age = 27
+let isMarried = false
+let something = null
+let Me = undefined
+
+// // console.log(`${typeof(name)}, ${typeof(Age)}, ${typeof(isMarried)}, ${typeof(something)}, ${typeof(Me)}`)
+
+// const str = "44"
+// const int = 45
+
+// const strToInt = parseInt(str);
+// const intToStr = String(int);
+// console.log(typeof strToInt)
+// console.log(typeof intToStr)
+
